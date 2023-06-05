0f626e681d140c2e2ce54549f45bf4ecc8aabcc2
SergioEslava <sergiotraceslava@gmail.com>
2023-06-05 18:10:38 +0200

    Return lost tracks

--- insect/byte_tracker/ByteTrack/yolox/tracker/byte_tracker.py
+++ insect/byte_tracker/ByteTrack/yolox/tracker/byte_tracker.py
@@ -283,7 +283,7 @@ class BYTETracker(object):
         self.tracked_stracks, self.lost_stracks = remove_duplicate_stracks(self.tracked_stracks, self.lost_stracks)
         # get scores of lost tracks
         output_stracks = [track for track in self.tracked_stracks if track.is_activated]
-
+        output_stracks.extend(self.lost_stracks)
         return output_stracks
 
 
