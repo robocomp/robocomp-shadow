cfe30aeccf198ee2220a776070b25726ccbc283d
alfiTH <alex_alfi@hotmail.com>
2023-08-02 17:58:05 +0200

    VIEWER graph vibration updata, multiple data .csv, add result test and change sample amortiguacion_dura

--- components/component_test/test/graph.py
+++ components/component_test/test/graph.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
-import time
-import pandas
-import sys, numpy
+from random import sample
+import sys
+import os
+from tkinter import NO
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing
 
-def get_mov(t):
-    movimiento = {"Avance":10, "Giro": 16.25, "Lateral": 26.25, "Giro2":27.5, "Parada1":27.75,
-                "Avance Rapido":31.5, "Parada2":31.75, "Giro3":38, "Parada3":38.25, "Lateral Rapido": 42
-                , "Parada4":42.25, "Giro4": 43.25, "Diagonal":48.25, "Parada5":48.5, "Diagonal Rapido":52.25, 
-                "Giro5":53.5}
-    for key, value in movimiento.items():
-        if t<value:
+def get_movement(time):
+    movements = {"Forward": 10, "Turn": 16.25, "Sideways": 26.25, "Turn2": 27.5, "Stop1": 27.75,
+                 "Fast Forward": 31.5, "Stop2": 31.75, "Turn3": 38, "Stop3": 38.25, "Fast Sideways": 42,
+                 "Stop4": 42.25, "Turn4": 43.25, "Diagonal": 48.25, "Stop5": 48.5, "Fast Diagonal": 52.25,
+                 "Turn5": 53.5}
+    for key, value in movements.items():
+        if time < value:
             return key
-    return "Out Range"
+    return "Out of Range"
 
 def  test_fourier():
     ##Seno sintetico para validar transformada de fourier
@@ -40,69 +44,110 @@ def  test_fourier():
     plt.show()
 
 
+def plot_signal(title, signal, time_period, frequencies, windows_size, frecuencia_muestreo):
+    ### Spectrogram
+    _, eAxis = plt.subplots(3, 1)
+    plt.subplots_adjust(hspace=0.5)
 
-####################################################MAIN###############################################
-if len(sys.argv)==3:
-    #Cargamos los datos
-    file = pandas.read_csv(sys.argv[1], delimiter=";")
-    print(file)
+    for i in range(3):
+        eAxis[i].specgram(x=signal[i], Fs=frecuencia_muestreo)
+        eAxis[i].set_ylabel("Hz", fontsize=10)
+        eAxis[i].set_xlabel("Time second", fontsize=10)
+    plt.suptitle(title, fontsize=16)
+    
+    plt.show()
 
-    #Eliminamos la columna de tiempo
-    file = file.drop("tiempo",axis=1)
-    #file = file.drop("Unnamed: 0",axis=1)
-    print(file.max().max())
-    #Plot data
-    file.plot(subplots=True, kind="line", logy=False, grid=True, ylim=[file.max().max(), file.min().min()], title=sys.argv[1])
+    ##### Manual Fourier
+    figure, axis = plt.subplots(3, 1)
+    plt.subplots_adjust(hspace=0.75)
+    overlap = windows_size // 2
+    end = len(signal[0]) - windows_size
+
+    for t in range(0, end, overlap):
+        for i in range(3):
+            signal_window = signal[i][t:t + windows_size]
+            fourier_transform = np.fft.fft(signal_window) / len(signal_window)
+            fourier_transform = fourier_transform[range(int(len(signal_window) / 2))]
+
+            axis[i].cla()
+            axis[i].set_ylim(0, 3)
+            axis[i].set_title("Movement " + get_movement(t * time_period / 1000) + " / Window " + str(t * time_period / 1000) + " s to " + str((t + windows_size) * time_period / 1000) + " s", fontsize=10)
+            axis[i].set_xlabel("Hz", fontsize=10)
+            axis[i].set_xticks(np.arange(0, frequencies[-1], 10))
+            axis[i].set_yticks(np.arange(0, 3, 0.5))
+            axis[i].set_ylabel("Acceleration", fontsize=10)
+            axis[i].grid()
+            axis[i].plot(frequencies[1:], abs(fourier_transform[1:]))
+
+        plt.suptitle(title, fontsize=16)
+        plt.draw()
+        plt.pause(0.25)
+
+def process_csv_file(path, file, time_period, max_y, min_y, samples):
+    
+
+    # Plot data
+    file.plot(xlabel="Time second", ylabel="mm/s^2", subplots=True, kind="line", logy=False, grid=True, ylim=[min_y,max_y], title=path, xlim = [0, samples])
     plt.show()
 
     if 'ax' in file:
-
-        #Array con los datos x, y, z
-        signalFull = numpy.array(file.values).transpose()
-        frecuenciaMuestreo = 1000//float(sys.argv[2])
-        tVentana = 1 #tiempo de ventana en segundos
-        windowsSize = int(tVentana*frecuenciaMuestreo)
-        values = numpy.arange(int(windowsSize/2))
-        values = values * frecuenciaMuestreo / windowsSize
-        timePeriod = float(sys.argv[2])
+        # Array with x, y, z data
+        signal_full = np.array(file.values).transpose()
+        frecuencia_muestreo = 1000 // time_period
+        t_ventana = 1  # Window time in seconds
+        windows_size = int(t_ventana * frecuencia_muestreo)
+        values = np.arange(int(windows_size / 2))
+        values = values * frecuencia_muestreo / windows_size
         frequencies = values
 
+        plot_signal(path, signal_full, time_period, frequencies, windows_size, frecuencia_muestreo)
 
-        ### Espectograma
-        espectogram, eAxis = plt.subplots(3, 1)
-        plt.subplots_adjust(hspace=0.5)
+
+def process_csv_files(data_directory, time_period):
+    csv_files = [file for file in os.listdir(data_directory) if file.endswith('.csv')]
+
+    if not csv_files:
+        print("No CSV files found in the directory.")
+        return
+
+    Processes = []
+    files = {}
+    max = None
+    min = None
+    samples = 0
+    for csv_file in csv_files:
+        csv_file_path = os.path.join(data_directory, csv_file)
+        print(f"Processing file: {csv_file_path}")
+        # Load data
+        file = pd.read_csv(csv_file_path, delimiter=";")
+        print(file)
+
+        # Remove the "time" column
+        file = file.drop("tiempo", axis=1)
+
+        if max is None: max = file.max().max()
+        elif (aux:=file.max().max())>max: max = aux
         
-        for i in range(3):
-            eAxis[i].specgram(x=signalFull[i], Fs=frecuenciaMuestreo)
-            #eAxis[i].specgram(x=signalFull[i], Fs=frecuenciaMuestreo, window=windowsSize, NFFT=windowsSize)
-            eAxis[i].set_ylabel("Hz", fontsize=10)
-            eAxis[i].set_xlabel("Time second", fontsize=10)
-        plt.draw()
+        if min is None: min = file.min().min() 
+        elif (aux:=file.min().min())<min: min = aux
+        if (aux:=file.shape[0])>samples: samples = aux
 
+        files[csv_file_path] = file
+        
+    print("Max: ", max, "/Min: ", min, "/Num samples: ", samples)
+    # Start all Processes
+    for path, file in files.items():
+        proc = multiprocessing.Process(target=process_csv_file, args=(path, file, time_period, max, min, samples))
+        Processes.append(proc)
+        proc.start()
 
-        ##### Fourier manual
-        figure, axis = plt.subplots(3, 1)
-        plt.subplots_adjust(hspace=0.75)
-        overLap = windowsSize//2
-        end = len(signalFull[0]) - windowsSize
-        for t in range(0,end, overLap):
-            for i in range(3):
-                signal =  signalFull[i][t:t+windowsSize]
-                fourierTransform = numpy.fft.fft(signal)/len(signal)           # Normalize amplitude
-                fourierTransform = fourierTransform[range(int(len(signal)/2))]  # Exclude sampling frequency
-                # Create subplot
-                axis[i].cla() 
-                axis[i].set_ylim(0, 3)
-                axis[i].set_title("Movimiento "+get_mov(t*timePeriod/1000)+"/ /Ventana "+str(t*timePeriod/1000)+" s a "+str((t+windowsSize)*timePeriod/1000)+" s", fontsize=10)
-                axis[i].set_xlabel("Hz", fontsize=10)
-                axis[i].set_xticks(numpy.arange(0, frequencies[-1], 10))
-                axis[i].set_yticks(numpy.arange(0, 3, 0.5))
-                axis[i].set_ylabel("Acceleration", fontsize=10)
-                axis[i].grid()
-                axis[i].plot(frequencies[1:], abs(fourierTransform[1:]))
-            plt.draw()
-            plt.pause(0.25)
+    # Wait for all Processes to complete
+    for proc in Processes:
+        proc.join()
 
-    
+if len(sys.argv) == 2:
+    data_directory = sys.argv[1]
+    time_period = float(input("Enter the time period in ms: "))
+    process_csv_files(data_directory, time_period)
 else:
-    print("Falta la dirección del csv o periodo de muestreo en ms")
+    print("Please provide the directory path containing the CSV files.")
