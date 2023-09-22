468da1fd2b0fa12b414a00cea0581b9e49a4c515
alfiTH <alex_alfi@hotmail.com>
2023-09-22 14:25:53 +0200

    change autostart.sh, the rcnode now launch with absolute path

--- etc/autostart.sh
+++ etc/autostart.sh
@@ -2,17 +2,20 @@
 # Scrip de auto arraque del shadow,debera tener permisos con sudo chmod +x autostart.sh y el rcnode e introducirlo en el crontab como:
 # @reboot /home/robocomp/robocomp/components/robocomp-shadow/etc/autostart.sh
 
-
+echo "Wait 20 second"
 sleep 20
 # Arrancamos rcnode tiene que tener permisos de ejecucion
-rcnode &
+echo "Start rcnode"
+/home/robocomp/robocomp/tools/rcnode/rcnode.sh&
 # Ubicamos en componente de la base y ejecutamos
+echo "Start base"
 cd /home/robocomp/robocomp/components/robocomp-shadow/components/shadowbase
-src/shadowbase.py etc/config &
+src/shadowbase.py etc/config&
 # Ubicamos en componente del joystick y ejecutamos
 cd /home/robocomp/robocomp/components/robocomp-robolab/components/hardware/external_control/joystickpublish
 # Por fallo en identificar el mando se realizaran varios intentos siendo la ejecucion en primer plano
 for i in {1..10};do
     sleep 10
+    echo "Start joystickPublish"
     bin/JoystickPublish etc/config_shadow 
 done
