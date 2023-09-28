da5109918922a18425ad5c919eaa5f00faafaede
LucasBonilla26 <lubonilla@alumnos.unex.es>
2023-09-28 12:37:17 +0200

    controller web cleaned

--- insect/controller_web/src/specificworker.py
+++ insect/controller_web/src/specificworker.py
@@ -46,12 +46,15 @@ console = Console(highlight=False)
 class SpecificWorker(GenericWorker):
     def __init__(self, proxy_map, startup_check=False):
         super(SpecificWorker, self).__init__(proxy_map)
+
         self.Period = 33
+
         if startup_check:
             self.startup_check()
         else:
             self.timer.timeout.connect(self.compute)
             self.timer.start(self.Period)
+
         # Create queues and locks
         self.frame_queue = queue.Queue()
         self.frame_lock = threading.Lock()
@@ -66,6 +69,7 @@ class SpecificWorker(GenericWorker):
         self.last_time = time.time()
         self.target = -1
         self.fps = 0
+
         # Iniciar el servidor Flask en un nuevo hilo
         self.app = Flask(__name__)
 
@@ -97,34 +101,16 @@ class SpecificWorker(GenericWorker):
 
             box = request.json.get('box')
             if not box:
-                # print("ENTRAAAAA3")
-                # print("NO TARGET")
                 self.target = -1
-                # self.segmentatortrackingpub_proxy.setTrack(target)
                 return jsonify({'status': 'failure', 'reason': 'No box data'}), 400
 
             for b in self.boxes:
-                # print("id box", b.id)
-                # print("id BOXES", str(box.get('id')))
                 if b.id == box.get('id'):
                     self.target = b.id
-                    # print("HAY TARGET", target)
-
-            # try:
-            #     if target:
-            #         self.segmentatortrackingpub_proxy.setTrack((target))
-            #     else:
-            #         print("NO TARGET")
-            #         target = ifaces.RoboCompVisualElements.TObject()
-            #         target.id = -1
-            #         self.segmentatortrackingpub_proxy.setTrack(target)
-
-            # except Ice.Exception as e:
-            #     traceback.print_exc()
-            #     print(e)
-            #     return jsonify({'status': 'failure', 'reason': "str(e)"}), 500
 
             return jsonify({'status': 'success'})
+        
+        #                                                                Caja-blanca IP
         self.flask_thread = Thread(target=self.app.run, kwargs={'host': '192.168.50.249', 'port': 5000}) # '192.168.50.153' orin ip
         self.flask_thread.start()
 
@@ -142,34 +128,27 @@ class SpecificWorker(GenericWorker):
 
     @QtCore.Slot()
     def compute(self):
-        t1 = time.time()
         image = None
 
         try:
-            t2 = time.time()
             image = self.camera360rgb_proxy.getROI(-1, -1, -1, -1, -1, -1)
         except:
             print("Camara no responde")
 
         try:
-            t3 = time.time()
             self.boxes = self.visualelements_proxy.getVisualObjects([])
             boxes = self.adapt_bbox(self.boxes, image)
         except:
             print("No Objects")
 
         for b in self.boxes:
-            # print("id box", b.id)
-            # print("id BOXES", str(box.get('id')))
             if b.id == self.target:
                 target = b
-                # print("HAY TARGET", target)
-
+                
         try:
             if self.target != -1:
                 self.segmentatortrackingpub_proxy.setTrack((target))
             else:
-                print("NO TARGET")
                 target = ifaces.RoboCompVisualElements.TObject()
                 target.id = -1
                 self.segmentatortrackingpub_proxy.setTrack(target)
@@ -178,9 +157,7 @@ class SpecificWorker(GenericWorker):
             traceback.print_exc()
             print(e)
 
-        t4 = time.time()
         self.create_queues(image, boxes)
-        #print("TIEMPO", (time.time()-t1) *1000)
 
         return True
 
@@ -189,7 +166,6 @@ class SpecificWorker(GenericWorker):
         boxes = []
         for obj in objects:
             # Obten la informacion de la ROI
-            
             roi = obj.image.roi
             final_xsize = roi.finalxsize
             final_ysize = roi.finalysize
@@ -197,6 +173,7 @@ class SpecificWorker(GenericWorker):
             roi_ycenter = roi.ycenter
             roi_xsize = roi.xsize
             roi_ysize = roi.ysize
+
             # Calcula el factor de escala y offset
             x_roi_offset = roi_xcenter - roi_xsize / 2
             y_roi_offset = roi_ycenter - roi_ysize / 2
@@ -212,6 +189,7 @@ class SpecificWorker(GenericWorker):
             # Crea una nueva caja redimensionada y añádela a la lista
             new_box = {'x': left, 'y': top, 'width': right - left, 'height': bot - top, 'id': obj.id}
             boxes.append(new_box)
+
         return boxes
 
     def create_queues(self, image, boxes):
@@ -220,7 +198,7 @@ class SpecificWorker(GenericWorker):
                 frame = np.frombuffer(image.image, dtype=np.uint8).reshape((image.height, image.width, 3))
                 self.frame_box_queue.put((frame, boxes))
             else:
-                # descarta la imagen más antigua si la cola está demasiado llena
+                # Discard the oldest image if the queue is full
                 self.frame_box_queue.get()
                 frame = np.frombuffer(image.image, dtype=np.uint8).reshape((image.height, image.width, 3))
                 self.frame_box_queue.put((frame, boxes))
@@ -244,11 +222,6 @@ class SpecificWorker(GenericWorker):
                     y = int(box['y'] * resolution[1] / original_height)
                     width = int(box['width'] * resolution[0] / original_width)
                     height = int(box['height'] * resolution[1] / original_height)
-                    # Redimensiona solo a 640 (Sergio)
-                    # box['x'] = int(box['x'] * 640 / original_width)
-                    # box['y'] = int(box['y'] * 640 / original_height)
-                    # box['width'] = int(box['width'] * 640 / original_width)
-                    # box['height'] = int(box['height'] * 640 / original_height)
 
                     # Redimensiona a la resolución especificada
                     box['x'] = int(box['x'] * resolution[0] / original_width)
@@ -261,7 +234,6 @@ class SpecificWorker(GenericWorker):
                     # Dibujar el bounding box en la imagen redimensionada
                     frame = cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
 
-                #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 _, buffer = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
                 frame = buffer.tobytes()
@@ -283,9 +255,7 @@ class SpecificWorker(GenericWorker):
         if time.time() - self.last_time > 1:
             self.last_time = time.time()
             cur_period = int(1000./self.cont)
-            #delta = (-1 if (period - cur_period) < -1 else (1 if (period - cur_period) > 1 else 0))
             print("Freq:", self.cont, "ms. Curr period:", cur_period)
-            #self.thread_period = np.clip(self.thread_period+delta, 0, 200)
             self.cont = 0
         else:
             self.cont += 1
