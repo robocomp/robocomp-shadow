92a369402bc949d66e0fb5d51dbf6aa80179a3ad
NoeZC <nzapatac@alumnos.unex.es>
2023-11-17 14:06:44 +0100

    pybullet model with real-time people insertion

---
+++ components/model/src/PersonManager.py
@@ -0,0 +1,136 @@
+import pybullet
+import time
+
+class PersonManager:
+    def __init__(self, physics_client_id, _pybullet):
+        self.pybullet = _pybullet
+        self.physics_client_id = physics_client_id
+        self.persons = {}
+        self.last_velocities = {}
+        self.last_orientation = {}
+        self.velocity_history = {}  # Historial de velocidades para cada persona
+        self.observation_count = {}
+        self.M = 15 #Número de frames pre-inserción persona.
+        self.N = 10  # Número de muestras a mantener
+
+        self.last_seen = {}  # Nuevo: Registro del último momento en que se vio a cada persona
+        self.disappearance_time = 4.5  # Tiempo en segundos para considerar desaparecida a una persona
+
+    def add_person(self, person_name, position, orientation, size):
+
+        # Cargar el modelo URDF
+        mass = 0.5
+        if self.observation_count[person_name] >= self.M:
+            person_id = self.pybullet.createCollisionShape(self.pybullet.GEOM_BOX, halfExtents=size, physicsClientId=self.physics_client_id)
+            visual_shape_id = self.pybullet.createVisualShape(self.pybullet.GEOM_BOX, halfExtents=size, physicsClientId=self.physics_client_id)
+
+            person_body_id = self.pybullet.createMultiBody(baseMass=mass,
+                                               baseCollisionShapeIndex=person_id,
+                                               baseVisualShapeIndex=visual_shape_id,
+                                               basePosition=position,
+                                               physicsClientId=self.physics_client_id)
+            # Ruta al archivo URDF
+            self.persons[person_name] = person_body_id
+            return person_body_id
+        else:
+            return None
+
+    def remove_person(self, person_name):
+        if person_name in self.persons:
+            self.pybullet.removeBody(self.persons[person_name], physicsClientId=self.physics_client_id)
+            del self.persons[person_name]
+
+    def change_velocity(self, person_name, velocity, pose, orientation):
+        if person_name in self.persons:
+            self.pybullet.resetBaseVelocity(self.persons[person_name], linearVelocity=velocity, physicsClientId=self.physics_client_id)
+            pos, _ = self.pybullet.getBasePositionAndOrientation(self.persons[person_name])
+            # self.pybullet.resetBasePositionAndOrientation(self.persons[person_name], pose, orientation, physicsClientId=self.physics_client_id )
+
+
+    def get_person_position(self, person_name):
+        if person_name in self.persons:
+            person_body_id = self.persons[person_name]
+            position, orientation = self.pybullet.getBasePositionAndOrientation(person_body_id, physicsClientId=self.physics_client_id)
+            return position  # Devuelve solo la posición, ignorando la orientación
+        return None  # Devuelve None si la persona no existe en la simulación
+
+    def update_person(self, person_name, position, orientation, velocity):
+        if person_name in self.persons:
+            person_body_id = self.persons[person_name]
+            self.pybullet.resetBasePositionAndOrientation(person_body_id, position, orientation,
+                                              physicsClientId=self.physics_client_id)
+            self.pybullet.resetBaseVelocity(person_body_id, linearVelocity=velocity, physicsClientId=self.physics_client_id)
+            # Actualizar historial de velocidades
+            if person_name not in self.velocity_history:
+                self.velocity_history[person_name] = []
+            self.velocity_history[person_name].append(velocity)
+            if len(self.velocity_history[person_name]) > self.N:
+                self.velocity_history[person_name].pop(0)
+            self.last_orientation[person_name] = orientation
+
+            # Actualizar el último momento visto
+            self.last_seen[person_name] = time.time()
+
+    def apply_last_velocity(self, person_name):
+        if person_name in self.persons:
+            person_body_id = self.persons[person_name]
+
+            # Usar el promedio de las últimas N velocidades
+            if person_name in self.velocity_history and self.velocity_history[person_name]:
+                average_velocity = [sum(vals) / len(vals) for vals in zip(*self.velocity_history[person_name])]
+                print("AVG", average_velocity)
+                pose, orientation = self.pybullet.getBasePositionAndOrientation(person_body_id)
+                self.pybullet.resetBasePositionAndOrientation(person_body_id, pose, self.last_orientation[person_name],
+                                                  physicsClientId=self.physics_client_id)
+                self.pybullet.resetBaseVelocity(person_body_id, linearVelocity=average_velocity,
+                                    physicsClientId=self.physics_client_id)
+
+    def remove_disappeared_persons(self):
+        current_time = time.time()
+        for person_name in list(self.last_seen.keys()):
+            if current_time - self.last_seen[person_name] > self.disappearance_time:
+                # La persona ha desaparecido durante demasiado tiempo, eliminarla
+                if person_name in self.persons:
+                    self.pybullet.removeBody(self.persons[person_name], physicsClientId=self.physics_client_id)
+                    del self.persons[person_name]
+                del self.last_seen[person_name]
+                if person_name in self.velocity_history:
+                    del self.velocity_history[person_name]
+                if person_name in self.last_orientation:
+                    del self.last_orientation[person_name]
+                if person_name in self.last_velocities:
+                    del self.last_velocities[person_name]
+
+            # # Aplicar la última velocidad conocida
+            # if person_name in self.last_velocities:
+            #     velocity = self.last_velocities[person_name]
+            #     pose , _ = self.pybullet.getBasePositionAndOrientation(person_body_id)
+            #     self.pybullet.resetBasePositionAndOrientation(person_body_id, pose, self.last_orientation[person_name],
+            #                                       physicsClientId=self.physics_client_id)
+            #     self.pybullet.resetBaseVelocity(person_body_id, linearVelocity=velocity, physicsClientId=self.physics_client_id)
+
+
+    # def add_person(self, person_name, position, qorientation, size, mass=0.5):
+    #
+    #     # person_id = self.pybullet.createCollisionShape(self.pybullet.GEOM_BOX, halfExtents=size, physicsClientId=self.physics_client_id)
+    #     # visual_shape_id = self.pybullet.createVisualShape(self.pybullet.GEOM_BOX, halfExtents=size, physicsClientId=self.physics_client_id)
+    #
+    #     urdf_path = '/home/robolab/.local/lib/python3.10/site-packages/pybullet_data/humanoid/humanoid.urdf'
+    #
+    #     body = self.pybullet.loadURDF(urdf_path, basePosition=[0,0,0],
+    #                       baseOrientation=[0,0,0,0], globalScaling=1)
+    #
+    #     person_id = self.pybullet.createCollisionShape(body, halfExtents=[1,1,1], physicsClientId=self.physics_client_id)
+    #     visual_shape_id = self.pybullet.createVisualShape(body, halfExtents=[1,1,1], physicsClientId=self.physics_client_id)
+    #
+    #     person_body_id = self.pybullet.createMultiBody(baseMass=mass,
+    #                                        baseCollisionShapeIndex=person_id,
+    #                                        baseVisualShapeIndex=visual_shape_id,
+    #                                        basePosition=position,
+    #                                        physicsClientId=self.physics_client_id)
+    #
+    #
+    #     self.persons[person_name] = person_body_id
+    #     # Actualizar el último momento visto
+    #     self.last_seen[person_name] = time.time()
+    #     return person_body_id
---
+++ components/model/src/specificworker.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2023 by YOUR NAME HERE
+#
+#    This file is part of RoboComp
+#
+#    RoboComp is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    RoboComp is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from PySide2.QtCore import QTimer
+from PySide2.QtWidgets import QApplication
+from rich.console import Console
+from genericworker import *
+import interfaces as ifaces
+
+import pybullet as p
+import time
+import pybullet_data
+import math
+from PersonManager import PersonManager
+
+sys.path.append('/opt/robocomp/lib')
+console = Console(highlight=False)
+
+
+
+
+
+
+class TRoi:
+    def __init__(self, xcenter, ycenter, xsize, ysize, finalxsize, finalysize):
+        self.xcenter = xcenter
+        self.ycenter = ycenter
+        self.xsize = xsize
+        self.ysize = ysize
+        self.finalxsize = finalxsize
+        self.finalysize = finalysize
+
+class TObject:
+    def __init__(self, id, object_type, left, top, right, bot, score, depth, x, y, z, metrics):
+        self.id = id
+        self.type = object_type
+        self.left = left
+        self.top = top
+        self.right = right
+        self.bot = bot
+        self.score = score
+        self.depth = depth
+        self.x = x
+        self.y = y
+        self.z = z
+        self.metrics = metrics
+        # Image and person attributes omitted for simplicity
+
+
+class SpecificWorker(GenericWorker):
+    def __init__(self, proxy_map, startup_check=False):
+        super(SpecificWorker, self).__init__(proxy_map)
+        self.Period = 30
+
+        if startup_check:
+            self.startup_check()
+        else:
+            self.timer.timeout.connect(self.compute)
+            self.timer.start(self.Period)
+
+    def __del__(self):
+        """Destructor"""
+
+    def setParams(self, params):
+
+        # BULLET
+        self.objects = []
+        self.physics_client = p.connect(p.GUI)
+
+        p.setTimeStep(1/50,0)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        planeId = p.loadURDF('plane.urdf')
+        p.setGravity(0, 0, -9.81)
+        self.person_manager = PersonManager(self.physics_client, p)
+
+        self.VisualObjectsSynth = []
+        self.height = 1.6
+
+        return True
+
+    @QtCore.Slot()
+    def compute(self):
+        t1= time.time()
+        try:
+            self.objects = self.visualelements_proxy.getVisualObjects(self.objects)
+        except:
+            print("Proxy error")
+        t2= time.time()
+        self.update_simulation(self.objects, 1)
+        t3 = time.time()
+        p.stepSimulation()
+        t4 = time.time()
+        # print("TIME",t2-t1,t3-t2,t4-t3,t4-t1)
+
+        return True
+
+    def startup_check(self):
+        print(f"Testing RoboCompVisualElements.TRoi from ifaces.RoboCompVisualElements")
+        test = ifaces.RoboCompVisualElements.TRoi()
+        print(f"Testing RoboCompVisualElements.TObject from ifaces.RoboCompVisualElements")
+        test = ifaces.RoboCompVisualElements.TObject()
+        QTimer.singleShot(200, QApplication.instance().quit)
+
+    def update_simulation(self, received_objects, k):
+
+        received_ids = set(obj.id for obj in received_objects)
+
+        self.person_manager.remove_disappeared_persons()
+
+        for received_obj in received_objects:
+            # Encuentra el objeto correspondiente en VisualObjectsSynth
+            simulated_obj = next((obj for obj in self.VisualObjectsSynth if obj.id == received_obj.id), None)
+
+            if simulated_obj:
+                # Obtener la posición actual del objeto simulado en pybullet
+                current_position = self.person_manager.get_person_position(simulated_obj.id)
+
+                if current_position:
+                    # Calcular la diferencia de posición (vector de error)
+                    dx = received_obj.x / 1000 - current_position[0]
+                    dy = received_obj.y / 1000 - current_position[1]
+                    dz = received_obj.z / 1000 - current_position[2]
+                    dz = 0
+
+                    # print("DX/DY/DZ: ", dx, dy, dz)
+                    # Calcular el vector de velocidad basado en el error y el factor k
+                    k = 33
+                    velocity = [k * dx, k * dy, k * dz]
+
+                    # pose = [received_obj.x / 1000, received_obj.y / 1000, received_obj.z / 1000]
+                    quaternions = self.rotation_z_to_quaternion(received_obj.person.orientation)
+
+                    # Actualizar la posición y la velocidad del objeto simulado en pybullet
+                    self.person_manager.update_person(simulated_obj.id, current_position, quaternions,
+                                                 velocity)
+                # Actualizar la posición del objeto simulado
+                simulated_obj.x, simulated_obj.y, simulated_obj.z = received_obj.x, received_obj.y, received_obj.z
+            else: #No insertado en el sistema
+                person_name = received_obj.id
+
+                # Incrementa el conteo de observaciones
+                if person_name in self.person_manager.observation_count:
+                    self.person_manager.observation_count[person_name] += 1
+                else:
+                    self.person_manager.observation_count[person_name] = 1
+
+                # Si el objeto es una persona, no hay ninguna en pybullet y observaciones > M crea una nueva
+                if not any(obj.type == "person" for obj in self.VisualObjectsSynth) and self.person_manager.observation_count[person_name] >= self.person_manager.M:
+                    orientation = self.rotation_z_to_quaternion(received_obj.person.orientation)
+                    person_id = self.person_manager.add_person(received_obj.id,
+                                                          [received_obj.x / 1000, received_obj.y / 1000, self.height], orientation, (0.3, 0.15, self.height))
+                    self.VisualObjectsSynth.append(received_obj)
+
+        # Luego, para cualquier persona en la simulación que no esté en received_objects, aplicar la última velocidad
+        for simulated_obj in self.VisualObjectsSynth:
+            if simulated_obj.id not in received_ids:
+                self.person_manager.apply_last_velocity(simulated_obj.id)
+
+
+    def calculate_velocity(self, simulated_obj, received_obj, k):
+        # Calcula la diferencia de posición en cada eje
+        dx = received_obj.x - simulated_obj.x
+        dy = received_obj.y - simulated_obj.y
+        dz = received_obj.z - simulated_obj.z
+
+        # Escala la diferencia por k para obtener la velocidad
+        velocity_x = k * dx
+        velocity_y = k * dy
+        velocity_z = k * dz
+
+        return [velocity_x, velocity_y, velocity_z]
+
+
+    def rotation_z_to_quaternion(self, theta):
+        # Calcular los componentes del cuaternión
+        w = math.cos(theta / 2)
+        z = math.sin(theta / 2)
+
+        # El cuaternión para una rotación alrededor del eje Z
+        return [0, 0, z, w]
+
+    ######################
+    # From the RoboCompVisualElements you can call this methods:
+    # self.visualelements_proxy.getVisualObjects(...)
+
+    ######################
+    # From the RoboCompVisualElements you can use this types:
+    # RoboCompVisualElements.TRoi
+    # RoboCompVisualElements.TObject
+
+
+
