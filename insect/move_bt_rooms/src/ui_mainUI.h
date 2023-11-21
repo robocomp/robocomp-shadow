bf3984c6ffa24445d14dabb9db051df6381307a8
LucasBonilla26 <lubonilla@alumnos.unex.es>
2023-11-21 11:33:14 +0100

    new component to move the robot between rooms using behaviortrees (inprogress)

---
+++ insect/move_bt_rooms/src/ui_mainUI.h
@@ -0,0 +1,58 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainUI.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINUI_H
+#define UI_MAINUI_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QVBoxLayout>
+QT_BEGIN_NAMESPACE
+
+class Ui_guiDlg
+{
+public:
+
+    QVBoxLayout *verticalLayout;
+    QFrame *frame;
+
+    void setupUi(QWidget *guiDlg)
+    {
+        if (guiDlg->objectName().isEmpty())
+            guiDlg->setObjectName(QString::fromUtf8("guiDlg"));
+        guiDlg->resize(800, 600);
+
+        verticalLayout = new QVBoxLayout(guiDlg);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        frame = new QFrame(guiDlg);
+        frame->setObjectName(QString::fromUtf8("frame"));
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+        verticalLayout->addWidget(frame);
+
+        retranslateUi(guiDlg);
+
+        QMetaObject::connectSlotsByName(guiDlg);
+    } // setupUi
+
+    void retranslateUi(QWidget *guiDlg)
+    {
+        guiDlg->setWindowTitle(QCoreApplication::translate("guiDlg", "move_bt_rooms", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class guiDlg: public Ui_guiDlg {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINUI_H
