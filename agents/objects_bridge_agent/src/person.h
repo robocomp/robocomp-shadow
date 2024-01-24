37b2365bffdd17503d7f9aaab507259ad4d10b64
GeraGrind96 <geragrind96@gmail.com>
2024-01-24 11:22:11 +0100

    Objects bridge agent added. Minor changes.

---
+++ agents/objects_bridge_agent/src/person.h
@@ -0,0 +1,56 @@
+//
+// Created by robolab on 1/16/24.
+//
+
+#ifndef PEOPLE_PATH_PREDICTOR_PERSON_H
+#define PEOPLE_PATH_PREDICTOR_PERSON_H
+
+#include "abstract_graphic_viewer/abstract_graphic_viewer.h"
+#include <VisualElementsPub.h>
+#include <Gridder.h>
+#include <Eigen/Eigen>
+
+class Person
+{
+    private:
+        RoboCompVisualElementsPub::TObject target;
+        QGraphicsEllipseItem *item = nullptr;
+        bool is_target = false;
+        std::chrono::high_resolution_clock::time_point insertion_time, last_update_time;
+
+        // Pilar Cone
+        QPolygonF pilar_cone;
+        RoboCompVisualElementsPub::TObjects objects_inside_pilar_cone;
+
+        // Gridder proxy pointer
+        RoboCompGridder::GridderPrxPtr gridder_proxy;
+
+        // Paths to visual elements
+        std::vector<std::pair<int, std::vector<Eigen::Vector2f>>> paths;
+
+    public:
+        Person(RoboCompGridder::GridderPrxPtr g_proxy);
+        Person();
+        void init_item(QGraphicsScene *scene, float x, float y, float angle, float cone_radius, float cone_angle);
+        void set_person_data(RoboCompVisualElementsPub::TObject person);
+        void update_attributes(const RoboCompVisualElementsPub::TObjects &list);
+        std::optional<float> get_attribute(const std::string &attribute_name) const;
+        bool set_attribute(const std::string &attribute_name, float value) ;
+        void is_inside_pilar_cone(const RoboCompVisualElementsPub::TObjects &list);
+        std::optional<std::pair<int, std::vector<Eigen::Vector2f>>> search_for_paths(const RoboCompVisualElementsPub::TObject &object);
+        void set_target_element(bool value);
+        int get_id() const;
+        QGraphicsItem* get_item() const;
+        RoboCompVisualElementsPub::TObject get_target() const;
+        bool is_target_element() const;
+        void set_updated(bool value);
+        void update_last_update_time();
+        std::chrono::high_resolution_clock::time_point get_last_update_time() const;
+        void set_insertion_time();
+        std::chrono::high_resolution_clock::time_point get_insertion_time() const;
+        void draw_paths(QGraphicsScene *scene, bool erase_only, bool wanted_person) const;
+        void remove_item(QGraphicsScene *scene);
+        void print() const;
+};
+
+#endif //PEOPLE_PATH_PREDICTOR_PERSON_H
---
+++ agents/objects_bridge_agent/src/ui_mainUI.h
@@ -0,0 +1,185 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainUI.ui'
+**
+** Created by: Qt User Interface Compiler version 6.2.4
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINUI_H
+#define UI_MAINUI_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLCDNumber>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_guiDlg
+{
+public:
+    QVBoxLayout *verticalLayout_2;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout_4;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLCDNumber *lcdNumber_hz;
+    QPushButton *pushButton_stop;
+    QSpacerItem *horizontalSpacer;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_2;
+    QLCDNumber *lcdNumber_people;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_3;
+    QLCDNumber *lcdNumber_room;
+    QHBoxLayout *horizontalLayout_5;
+    QLabel *label_4;
+    QLCDNumber *lcdNumber_elapsed;
+    QFrame *frame;
+
+    void setupUi(QWidget *guiDlg)
+    {
+        if (guiDlg->objectName().isEmpty())
+            guiDlg->setObjectName(QString::fromUtf8("guiDlg"));
+        guiDlg->resize(800, 600);
+        verticalLayout_2 = new QVBoxLayout(guiDlg);
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        label = new QLabel(guiDlg);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setFrameShape(QFrame::StyledPanel);
+
+        horizontalLayout->addWidget(label);
+
+        lcdNumber_hz = new QLCDNumber(guiDlg);
+        lcdNumber_hz->setObjectName(QString::fromUtf8("lcdNumber_hz"));
+        QFont font;
+        font.setFamilies({QString::fromUtf8("Ubuntu")});
+        font.setPointSize(12);
+        font.setBold(true);
+        lcdNumber_hz->setFont(font);
+
+        horizontalLayout->addWidget(lcdNumber_hz);
+
+
+        horizontalLayout_4->addLayout(horizontalLayout);
+
+        pushButton_stop = new QPushButton(guiDlg);
+        pushButton_stop->setObjectName(QString::fromUtf8("pushButton_stop"));
+        pushButton_stop->setCheckable(true);
+        pushButton_stop->setChecked(true);
+
+        horizontalLayout_4->addWidget(pushButton_stop);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        label_2 = new QLabel(guiDlg);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setFrameShape(QFrame::StyledPanel);
+
+        horizontalLayout_2->addWidget(label_2);
+
+        lcdNumber_people = new QLCDNumber(guiDlg);
+        lcdNumber_people->setObjectName(QString::fromUtf8("lcdNumber_people"));
+        QFont font1;
+        font1.setBold(true);
+        lcdNumber_people->setFont(font1);
+
+        horizontalLayout_2->addWidget(lcdNumber_people);
+
+
+        horizontalLayout_4->addLayout(horizontalLayout_2);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        label_3 = new QLabel(guiDlg);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setFrameShape(QFrame::StyledPanel);
+
+        horizontalLayout_3->addWidget(label_3);
+
+        lcdNumber_room = new QLCDNumber(guiDlg);
+        lcdNumber_room->setObjectName(QString::fromUtf8("lcdNumber_room"));
+        lcdNumber_room->setFont(font1);
+
+        horizontalLayout_3->addWidget(lcdNumber_room);
+
+        horizontalLayout_5 = new QHBoxLayout();
+        horizontalLayout_5->setObjectName(QString::fromUtf8("horizontalLayout_5"));
+        label_4 = new QLabel(guiDlg);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setFrameShape(QFrame::StyledPanel);
+
+        horizontalLayout_5->addWidget(label_4);
+
+        lcdNumber_elapsed = new QLCDNumber(guiDlg);
+        lcdNumber_elapsed->setObjectName(QString::fromUtf8("lcdNumber_elapsed"));
+        lcdNumber_elapsed->setFont(font1);
+
+        horizontalLayout_5->addWidget(lcdNumber_elapsed);
+
+
+        horizontalLayout_3->addLayout(horizontalLayout_5);
+
+
+        horizontalLayout_4->addLayout(horizontalLayout_3);
+
+
+        verticalLayout->addLayout(horizontalLayout_4);
+
+        frame = new QFrame(guiDlg);
+        frame->setObjectName(QString::fromUtf8("frame"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(frame->sizePolicy().hasHeightForWidth());
+        frame->setSizePolicy(sizePolicy);
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+
+        verticalLayout->addWidget(frame);
+
+
+        verticalLayout_2->addLayout(verticalLayout);
+
+
+        retranslateUi(guiDlg);
+
+        QMetaObject::connectSlotsByName(guiDlg);
+    } // setupUi
+
+    void retranslateUi(QWidget *guiDlg)
+    {
+        guiDlg->setWindowTitle(QCoreApplication::translate("guiDlg", "intention_predictor", nullptr));
+        label->setText(QCoreApplication::translate("guiDlg", "Hz", nullptr));
+        pushButton_stop->setText(QCoreApplication::translate("guiDlg", "STOP", nullptr));
+        label_2->setText(QCoreApplication::translate("guiDlg", "People", nullptr));
+        label_3->setText(QCoreApplication::translate("guiDlg", "Room", nullptr));
+        label_4->setText(QCoreApplication::translate("guiDlg", "Elapsed", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class guiDlg: public Ui_guiDlg {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINUI_H
