43ebf84ec02138c6e67d5ed24a5db8f652f1b787
NoeZC <nzapatac@alumnos.unex.es>
2024-05-23 11:09:25 +0200

    Segmentation fault fixed, number of iterations added to config file

--- components/g2o_cpp/src/specificmonitor.cpp
+++ components/g2o_cpp/src/specificmonitor.cpp
@@ -86,10 +86,13 @@ bool SpecificMonitor::sendParamsToWorker(RoboCompCommonBehavior::ParameterList p
 ///We need to supply a list of accepted values to each call
 void SpecificMonitor::readConfig(RoboCompCommonBehavior::ParameterList &params )
 {
-//	RoboCompCommonBehavior::Parameter aux;
-//	aux.editable = true;
-//	configGetString( "","InnerModelPath", aux.value, "nofile");
-//	params["InnerModelPath"] = aux;
+	RoboCompCommonBehavior::Parameter aux;
+	aux.editable = true;
+
+    //Read parameter iterations from config file
+	configGetString( "","Iterations", aux.value, "200");
+	params["Iterations"] = aux;
+
 }
 
 //Check parameters and transform them to worker structure
