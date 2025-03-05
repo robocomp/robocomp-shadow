237f6da2909246dfc0b717620cff2685c8ca7040
Jorge Calderon <jcalderoxp@alumnos.unex.es>
2025-03-05 13:38:37 +0100

    Fixed the load of the robot

--- components/pybullet_bump/src/genericworker.py
+++ components/pybullet_bump/src/genericworker.py
@@ -41,6 +41,7 @@ class GenericWorker(QtCore.QObject):
     def __init__(self, mprx):
         super(GenericWorker, self).__init__()
 
+        self.omnirobot_proxy = mprx["OmniRobotProxy"]
 
         self.mutex = QtCore.QMutex()
         self.Period = 30
